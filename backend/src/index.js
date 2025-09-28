2980bedf52ea9d69a1563cbd3faab1b9041e1284
Nishith-062 <nishith.cbit@gmail.com>
2025-09-28 15:09:08 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,14 +11,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-console.log("JWT_SECRETKEY:", process.env.JWT_SECRETKEY);
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
     origin: ["http://localhost:4173", "http://localhost:5173","https://veda-gamma.vercel.app"], // allow both
   credentials: true,
 }));
+
+console.log("JWT_SECRETKEY:", process.env.JWT_SECRETKEY);
+
+app.use(express.json());
+app.use(cookieParser());
+
 // app.use(cors());
 
 app.use("/api/user", userRoutes);
