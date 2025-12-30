20cf3c06e5df79f93af3ebbd453d85cde189823a
Itz_Secret <nishithvadla@gmail.com>
2025-12-30 22:22:16 +0530

    final

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: [
     "https://veda-gamma.vercel.app",
-    "http://localhost:5173",
+    "http://localhost:5174",
     "http://localhost:4173"
 
   ],
